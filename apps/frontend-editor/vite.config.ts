af45d89ab814953f1fe8fba95caefe0f23de5843
adonisote <adonis.almagro@code.berlin>
2025-01-21 13:57:30 +0100

    SETUP 📦 Alias Paths & Proxy Backend Connection (#2532)

--- apps/frontend-editor/vite.config.ts
+++ apps/frontend-editor/vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: [
+        {
+          find: '@',
+          replacement: fileURLToPath(new URL('./src', import.meta.url)),
+        },
+      ],
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: false,
+        },
+      },
+    },
+  };
+});
