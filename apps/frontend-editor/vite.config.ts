e04e569a3d0c4013a27a2ea21b7dc56c4c265aef
adonisote <adonis.almagro@code.berlin>
2025-03-05 17:30:43 +0100

    SETUP 📦 Install tailwind (#2432)

--- apps/frontend-editor/vite.config.ts
+++ apps/frontend-editor/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     resolve: {
       alias: [
         {
