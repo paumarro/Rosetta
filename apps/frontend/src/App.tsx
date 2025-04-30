6d7f849a94aefd558f16b92c114371b13f4c7650
adonisote <adonis.almagro@code.berlin>
2025-04-30 11:01:02 +0300

    FEAT✨ Login page (#2416)

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -8,15 +8,17 @@ import { Routes, Route, Link } from 'react-router-dom';
 
 export default function App() {
   return (
-    <>
-      <nav>
+    <div className="min-h-screen bg-background">
+      <nav className="bg-fill-primary-weak p-4 flex gap-4">
         <Link to="/home">Home</Link>
         <Link to="/test">API Test</Link>
         <Link to="/login">Login</Link>
+        <div>
+          <form action="/api/login/logout" method="post">
+            <button type="submit">Logout</button>
+          </form>
+        </div>
       </nav>
-      <form action="/api/login/logout" method="post">
-        <button type="submit">Logout</button>
-      </form>
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/test" element={<ApiTest />} />
@@ -24,6 +26,6 @@ export default function App() {
         <Route path="/person" element={<Person />} />
         <Route path="/editor" element={<Editor />} />
       </Routes>
-    </>
+    </div>
   );
 }
--- apps/frontend/src/pages/Login.tsx
+++ apps/frontend/src/pages/Login.tsx
@@ -1,70 +1,88 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-interface LoginResponse {
-  token: string;
-}
-
-export default function Login() {
-  const [error, setError] = useState<string | null>(null);
-  const navigate = useNavigate();
-  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const username = formData.get('username');
-    const password = formData.get('password');
-
-    try {
-      const response = await fetch('/api/login/password', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-      });
-
-      if (response.ok) {
-        const data = (await response.json()) as LoginResponse;
-        localStorage.setItem('jwt', data.token); // Store the JWT
-        console.log('Login successful:', data);
-        void navigate('/person'); // Redirect to the home page
-      } else {
-        setError('Login failed. Please check your credentials.');
-      }
-    } catch (error) {
-      console.error('Error during login:', error);
-      setError('An error occurred. Please try again.');
-    }
-  };
-
+import { GalleryVerticalEnd } from 'lucide-react';
+import { LoginForm } from '@/components/login-form';
+export default function LoginPage() {
   return (
-    <>
-      <div className="container mx-auto p-4 bg-amber-300">
-        <h1>Sign in</h1>
-        <form
-          onSubmit={(event) => {
-            handleLogin(event).catch((error: unknown) => {
-              console.error('Error handling login:', error);
-            });
-          }}
-        >
-          <section>
-            <label htmlFor="username">Username</label>
-            <input
-              id="username"
-              name="username"
-              type="text"
-              required
-              autoFocus
-            />
-          </section>
-          <section>
-            <label htmlFor="password">Password</label>
-            <input id="password" name="password" type="password" required />
-          </section>
-          <button type="submit">Sign in</button>
-        </form>
-        {error && <p className="text-red-500 mt-2">{error}</p>}
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="#" className="flex items-center gap-2 self-center font-medium">
+          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+            <GalleryVerticalEnd className="size-4" />
+          </div>
+          Carbyte Technologies
+        </a>
+        <LoginForm />
       </div>
-    </>
+    </div>
   );
 }
+
+// import { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// interface LoginResponse {
+//   token: string;
+// }
+
+// export default function Login() {
+//   const [error, setError] = useState<string | null>(null);
+//   const navigate = useNavigate();
+//   const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     const formData = new FormData(event.currentTarget);
+//     const username = formData.get('username');
+//     const password = formData.get('password');
+
+//     try {
+//       const response = await fetch('/api/login/password', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ username, password }),
+//       });
+
+//       if (response.ok) {
+//         const data = (await response.json()) as LoginResponse;
+//         localStorage.setItem('jwt', data.token); // Store the JWT
+//         console.log('Login successful:', data);
+//         void navigate('/person'); // Redirect to the home page
+//       } else {
+//         setError('Login failed. Please check your credentials.');
+//       }
+//     } catch (error) {
+//       console.error('Error during login:', error);
+//       setError('An error occurred. Please try again.');
+//     }
+//   };
+
+//   return (
+//     <>
+//       <div className="container mx-auto p-4 bg-amber-300">
+//         <h1>Sign in</h1>
+//         <form
+//           onSubmit={(event) => {
+//             handleLogin(event).catch((error: unknown) => {
+//               console.error('Error handling login:', error);
+//             });
+//           }}
+//         >
+//           <section>
+//             <label htmlFor="username">Username</label>
+//             <input
+//               id="username"
+//               name="username"
+//               type="text"
+//               required
+//               autoFocus
+//             />
+//           </section>
+//           <section>
+//             <label htmlFor="password">Password</label>
+//             <input id="password" name="password" type="password" required />
+//           </section>
+//           <button type="submit">Sign in</button>
+//         </form>
+//         {error && <p className="text-red-500 mt-2">{error}</p>}
+//       </div>
+//     </>
+//   );
+// }
