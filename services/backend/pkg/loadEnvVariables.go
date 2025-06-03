352ae8d2fcd00f92e854dc0efdcc5c821ab50b7c
paumarro <pau.marro-schmitt@carbyte.de>
2025-06-03 22:21:33 +0200

    Refactor: Enhance LoadEnvVariables for Docker

---
+++ services/backend/pkg/loadEnvVariables.go
@@ -0,0 +1,26 @@
+package env
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnvVariables(envFile string) {
+	log.Println("Loading environment variables...")
+
+	// Check if running in a Docker container
+	if os.Getenv("RUNNING_IN_DOCKER") == "true" {
+		log.Println("Running inside Docker container. Skipping .env file loading.")
+		return
+	}
+
+	// Load .env file
+	err := godotenv.Load(envFile)
+	if err != nil {
+		log.Fatalf("Error loading %s file: %v", envFile, err)
+	}
+
+	log.Println("Environment variables loaded successfully!")
+}
