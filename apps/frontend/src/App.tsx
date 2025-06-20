927a098f81297411135f33fcb2058bdd55a84d5d
paumarro <pau.marro-schmitt@carbyte.de>
2025-06-20 17:53:39 +0200

    Refactor: Use Context hook for auth

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -6,42 +6,94 @@ import Editor from '@/pages/Editor';
 import WelcomePage from '@/pages/Welcome';
 import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { NavigationProvider } from '@/components/nav-provider';
 import PathDesigner from '@/components/dashboard/PathDesigner';
 import LearningPaths from '@/components/dashboard/LearningPaths';
 import CreateNewPath from '@/components/creator-studio/CreateNewPath';
+import { AuthProvider } from './contexts/AuthContext';
+import RequireAuth from './wrappers/RequireAuth'; 
+
 export default function App() {
   return (
-    <NavigationProvider>
       <div className="min-h-screen bg-background">
-        <Routes>
-          {/* Public route - anyone can access */}
-          <Route path="/login" element={<Login />} />
+        <AuthProvider>
+          <Routes>
+            {/* Public route - anyone can access */}
+            <Route path="/login" element={<Login />} />
 
-          {/* Protected routes - only logged-in users */}
-          <Route path="/welcome" element={<WelcomePage />} />
-          <Route path="/" element={<Home />} />
+            {/* Protected routes - only logged-in users */}
+            <Route
+              path="/welcome"
+              element={
+                <RequireAuth>
+                  <WelcomePage />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/"
+              element={
+                <RequireAuth>
+                  <Home />
+                </RequireAuth>
+              }
+            />
 
-          {/* Learning Hub sections */}
-          <Route path="/hub">
-            <Route path="learning-path" element={<LearningPaths />} />
-          </Route>
+            {/* Learning Hub sections */}
+            <Route path="/hub">
+              <Route
+                path="learning-path"
+                element={
+                  <RequireAuth>
+                    <LearningPaths />
+                  </RequireAuth>
+                }
+              />
+            </Route>
 
-          {/* Creator sections */}
-          <Route path="/creator">
-            <Route path="path-design" element={<PathDesigner />}></Route>
-            <Route path="path-design/create-new" element={<CreateNewPath />} />
-          </Route>
+            {/* Creator sections */}
+            <Route path="/creator">
+              <Route
+                path="path-design"
+                element={
+                  <RequireAuth>
+                    <PathDesigner />
+                  </RequireAuth>
+                }
+              />
+              <Route
+                path="path-design/create-new"
+                element={
+                  <RequireAuth>
+                    <CreateNewPath />
+                  </RequireAuth>
+                }
+              />
+            </Route>
 
-          {/* Test and not implemented routes */}
-          <Route path="/test" element={<ApiTest />} />
-          <Route path="/person" element={<Person />} />
-          <Route path="/editor" element={<Editor />} />
+            {/* Test and not implemented routes */}
+            <Route
+              path="/test"
+              element={
+                  <ApiTest />
+              }
+            />
+            <Route
+              path="/person"
+              element={
+                  <Person />
+              }
+            />
+            <Route
+              path="/editor"
+              element={
+                  <Editor />
+              }
+            />
 
-          {/* Catch-all redirect */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
+            {/* Catch-all redirect */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </AuthProvider>
       </div>
-    </NavigationProvider>
   );
 }
---
+++ apps/frontend/src/wrappers/RequireAuth.tsx
@@ -0,0 +1,21 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import { ReactNode } from "react";
+
+
+interface RequireAuthProps {
+  children: ReactNode; 
+}
+
+const RequireAuth: React.FC<RequireAuthProps>  = ({ children }) => {
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) return <p>Loading...</p>;
+
+  console.log("isAuthenticated:", isAuthenticated);
+
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
+
+export default RequireAuth;
