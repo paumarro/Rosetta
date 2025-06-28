bc0809ae50de880d3a57838344865ef0ef024384
adonisote <adonis.almagro@code.berlin>
2025-06-28 17:07:46 +0200

    SETUP 📦 Add morgan logging middleware (#2529)

--- services/backend-editor/src/index.ts
+++ services/backend-editor/src/index.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from "express";
+import morgan from 'morgan'
 
 const app = express()
 const PORT = process.env.PORT || 3001
 
+app.use(morgan("combined")) // use "dev" for development and "combined" for production
 app.get("/", (req: Request, res: Response) => {
   console.log("Requested: ", req.url)
   res.sendStatus(200)
