d0c9f89879ae35448705ec00999f7e3d54187641
adonisote <adonis.almagro@code.berlin>
2025-06-29 18:38:47 +0200

    SETUP 📦 Update eslint config to "flat" config format  (#2529)

--- services/backend-editor/eslint.config.js
+++ services/backend-editor/eslint.config.js
@@ -3,14 +3,42 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import json from "@eslint/json";
 import markdown from "@eslint/markdown";
-import { defineConfig } from "eslint/config";
 
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.node } },
-  tseslint.configs.recommended,
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
-  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/commonmark", extends: ["markdown/recommended"] },
-]);
+export default tseslint.config(
+  { ignores: ['dist', '.husky', 'node_modules', 'package-lock.json'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.strictTypeChecked],
+    files: ['**/*.{ts}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.node,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname
+      },
+    },
+    rules: {
+
+    },
+  },
+  {
+    extends: [js.configs.recommended],
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.node,
+    },
+  },
+  { files: ["**/*.json"], language: "json/json", extends: [json.configs.recommended] },
+  { files: ["**/*.jsonc", "**/tsconfig.json"], language: "json/jsonc", extends: [json.configs.recommended] },
+  {
+    files: ["**/*.md"],
+    plugins: { markdown },
+    language: "markdown/commonmark",
+    extends: [markdown.configs.recommended],
+    rules: {
+      "markdown/no-multiple-h1": "warn",
+    }
+  }
+);
