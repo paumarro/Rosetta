c488f355cc892994543985cfee13780231f343f8
adonisote <adonis.almagro@code.berlin>
2025-06-30 07:05:32 +0200

    FIX 🐛 Fix type issue on Port (#2529)

--- services/backend-editor/src/index.ts
+++ services/backend-editor/src/index.ts
@@ -1,16 +1,16 @@
-import express, { Request, Response } from "express";
-import morgan from 'morgan'
+import express, { Request, Response } from 'express';
+import morgan from 'morgan';
 
-const app = express()
-const PORT = process.env.PORT || 3001
+const app = express();
+const PORT: number = Number(process.env.PORT) || 3001;
 
-app.use(morgan("combined")) // use "dev" for development and "combined" for production
-app.get("/", (req: Request, res: Response) => {
-  console.log("Requested: ", req.url)
-  res.sendStatus(200)
-})
+app.use(morgan('combined')); // use "dev" for development and "combined" for production
+app.get('/', (req: Request, res: Response) => {
+  console.log('Requested: ', req.url);
+  res.sendStatus(200);
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
-export default app
+  console.log(`Server is running on port ${String(PORT)}`);
+});
+export default app;
