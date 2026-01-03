ce17524a297802314367101511fa42d764f73d1d
Pau Marro <pau.marro-schmitt@carbyte.de>
2026-01-03 11:50:09 +0100

    style (frontend): Capitalize Component file names

--- apps/frontend-editor/src/components/ui/AddNodeButton.tsx
+++ apps/frontend-editor/src/components/ui/AddNodeButton.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@/components/ui/button';
+import { Button } from '@/components/ui/Button';
 import { useCollaborativeStore } from '@/store/collaborationStore';
 import { Plus, RectangleHorizontal } from 'lucide-react';
 import {
--- apps/frontend-editor/src/pages/NotFound.tsx
+++ apps/frontend-editor/src/pages/NotFound.tsx
@@ -1,6 +1,6 @@
 import { useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
-import { Button } from '../components/ui/button';
+import { Button } from '../components/ui/Button';
 import bottomImage from '../assets/Dotted-Bg.png';
 
 const NotFound = () => {
